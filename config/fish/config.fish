2b3cfce62fb3ecf8aecbcb728b03b1cb6a13bf06
Raph-Rodrigues <raph.edits06@gmail.com>
2026-07-23 01:02:07 -0300

    Fixing&Adding: fixing error on the config.fish, adding virt-manager.fish shell script

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,62 +1,62 @@
 if test "$TERM" = xterm-kitty
-	set -gx TERM xterm-256color
+    set -gx TERM xterm-256color
 end
 
 set -gx XDG_DATA_DIRS $HOME/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
 
-if status is_interactive
-	# no greeting
-	set fish_greeting
+if status is-interactive
+    # no greeting
+    set fish_greeting
 
-	# use starship
-	if test "$TERM" != linux
-		starship init fish | source
-		enable_transience
-		function starship_transient_prompt_func
-			starship module character
-		end
-	end
+    # use starship
+    if test "$TERM" != linux
+        starship init fish | source
+        enable_transience
+        function starship_transient_prompt_func
+            starship module character
+        end
+    end
 
-	# aliases
-	alias clear "printf '\033[2J\033[[3J\033[1;1H'"
-	alias celar "printf '\033[2J\033[[3J\033[1;1H'"
-	alias claer "printf '\033[2J\033[[3J\033[1;1H'"
-	alias pamcan pacman
-	alias q 'qs -c ii'
-	if test "$TERM" != linux
-		alias ls 'eza -a --icons --group-directories-first'
-	end
-	if test "$TERM" = xterm-kitty
-		alias ssh 'kitten ssh'
-	end
+    # aliases
+    alias clear "printf '\033[2J\033[[3J\033[1;1H'"
+    alias celar "printf '\033[2J\033[[3J\033[1;1H'"
+    alias claer "printf '\033[2J\033[[3J\033[1;1H'"
+    alias pamcan pacman
+    alias q 'qs -c ii'
+    if test "$TERM" != linux
+        alias ls 'eza -a --icons --group-directories-first'
+    end
+    if test "$TERM" = xterm-kitty
+        alias ssh 'kitten ssh'
+    end
 
-	# fastfetch
-	fastfetch
+    # fastfetch
+    fastfetch
 
-	# ASDF configuration code
-	if test -z $ASDF_DATA_DIR
-	    set _asdf_shims "$HOME/.asdf/shims"
-	else
-	    set _asdf_shims "$ASDF_DATA_DIR/shims"
-	end
+    # ASDF configuration code
+    if test -z $ASDF_DATA_DIR
+        set _asdf_shims "$HOME/.asdf/shims"
+    else
+        set _asdf_shims "$ASDF_DATA_DIR/shims"
+    end
 
-	# Do not use fish_add_path (added in Fish 3.2) because it
-	# potentially changes the order of items in PATH
-	if not contains $_asdf_shims $PATH
-	    set -gx --prepend PATH $_asdf_shims
-	end
-	set --erase _asdf_shims
+    # Do not use fish_add_path (added in Fish 3.2) because it
+    # potentially changes the order of items in PATH
+    if not contains $_asdf_shims $PATH
+        set -gx --prepend PATH $_asdf_shims
+    end
+    set --erase _asdf_shims
 end
 
 # >>> conda initialize >>>
 if test -f $HOME/anaconda3/bin/conda
-	eval $HOME/anaconda3/bin/conda "shell.fish" hook $argv | source
+    eval $HOME/anaconda3/bin/conda "shell.fish" hook $argv | source
 else
-	if test -f "$HOME/anaconda3/etc/fish/conf.d/conda.fish"
-		. "$HOME/anaconda3/etc/fish/conf.d/conda.fish"
-	else
-		set -x PATH $HOME/anaconda3/bin/ $PATH
-	end
+    if test -f "$HOME/anaconda3/etc/fish/conf.d/conda.fish"
+        . "$HOME/anaconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH $HOME/anaconda3/bin/ $PATH
+    end
 end
 
 # <<< conda initialize <<<
---
+++ config/fish/functions/virt-manager.fish
@@ -0,0 +1,3 @@
+function virt-manager --wraps='/usr/bin/python3 /usr/bin/virt-manager' --description 'alias virt-manager=/usr/bin/python3 /usr/bin/virt-manager'
+    /usr/bin/python3 /usr/bin/virt-manager $argv
+end
